d2ac4313635a6a55f5003fa01fd1a03699ba1f0f
adyang94 <adyang94@yahoo.com>
2021-12-29 17:03:53 -0500

    creating todolist tsx

---
+++ frontend/src/components/TodoList.tsx
@@ -0,0 +1,19 @@
+import { Todo } from "../entities/Todo"
+
+type Props = {
+  todos: Todo[]
+}
+
+export const TodoList: React.FC<Props> = ({ todos }) => {
+  return(
+    <ul className="todo-list">
+      {
+        todos.map((todo, i) => {
+          return(
+            <li key={i}>{todo.title}</li>
+          )
+        })
+      }
+    </ul>
+  )
+}
