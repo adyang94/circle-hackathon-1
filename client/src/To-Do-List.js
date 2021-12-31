763bf33362310b10e224c0396dd5aa59aae32ff9
adyang94 <adyang94@yahoo.com>
2021-12-31 18:36:05 -0500

    adding functions in react from video part 11

--- client/src/To-Do-List.js
+++ client/src/To-Do-List.js
@@ -9,7 +9,7 @@ class ToDoList extends Component {
     super(props);
 
     this.state = {
-      tasks: "",
+      task: "",
       items: [],
     };
   }
@@ -18,6 +18,98 @@ class ToDoList extends Component {
     this.getTask();
   }
 
+  onChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value,
+    })
+  }
+
+  onSubmit
+
+  getTask = () => {
+    axios.get(endpoint + "/api/task").then((res) => {
+      if (res.data) {
+        this.setState({
+          items: res.data.map((item) => {
+            let color = "yellow";
+            let style = {
+              wordWrap: "break-word",
+            };
+
+            if (item.status){
+              color = "green";
+              style["textDecorationLine"] = "line-through";
+
+            }
+
+            return(
+              <Card key={item._id} color={color} fluid className="rough">
+                <Card.Content>
+                  <Card.Header textAlign="left">
+                    <div style={style}>{item.task}</div>
+                  </Card.Header>
+
+                  <Card.Meta>
+                    <Icon 
+                      name="check circle" 
+                      color="blue" 
+                      onClick ={() => this.updateTask(item._id)} 
+                    />
+                    <span style={{paddingRight: 10}}>Undo</span>
+
+                    <Icon 
+                      name="delete"
+                      color="red"
+                      onClick = {() => this.deleteTask(item._id)}
+                    />
+                    <span style={{paddingright:10}}>Delete</span>
+                  </Card.Meta>
+                </Card.Content>
+              </Card>
+            );
+          }),
+        });
+      } else {
+        this.setState({
+          items: [],
+        });
+      }
+    });
+  };
+
+  updateTask = (id) => {
+    axios.put(endpoint + "/api/task" +id, {
+      headers: {
+        "Content-Type":"application/x-www-form-urlencoded",
+      },
+    }).then((res) => {
+      console.log(res);
+      this.getTask();
+    })
+  }
+
+  undoTask = (id) => {
+    axios.put(endpoint + "api/undoTask" + id, {
+      headers: {
+        "Content-Type":"application/x-www-form-urlencoded",
+      },
+    }).then((res) => {
+      console.log(res);
+      this.getTask();
+    });
+  };
+
+  deleteTask = (id) => {
+    axios.delete(endpoint + "api/deleteTask" + id, {
+      header: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+    }).then((res) => {
+      console.log(res);
+      this.getTask();
+    });
+  };
+
   render () {
     return(
       <div>
@@ -25,6 +117,22 @@ class ToDoList extends Component {
           <Header className="header" as="h2" color="yellow">
             TO DO LIST
           </Header>
+        </div>
+        <div className="row">
+          <Form onSubmit={this.onSubmit}>
+            <Input
+              type="text"
+              name="task"
+              onChange={this.onChange}
+              value={this.state.task}
+              fluid
+              placeholder="Create Task"
+            />
+            {{/* button for creating task */}}
+          </Form>
+        </div>
+        <div className="row">
+          <Card.Group>{this.state.items}</Card.Group>
         </div>
       </div>
     );
--- server/go.mod
+++ server/go.mod
@@ -1,3 +1,11 @@
 module github.com/adyang94/react-go-todo-app
 
 go 1.17
+
+require (
+	github.com/gorilla/mux v1.8.0
+	github.com/joho/godotenv v1.4.0
+	go.mongodb.org/mongo-driver v1.8.1
+)
+
+require github.com/go-stack/stack v1.8.0 // indirect
