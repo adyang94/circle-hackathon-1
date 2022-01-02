0686daf3203a28c4d8755213fb7941e3e469ff9e
adyang94 <adyang94@yahoo.com>
2022-01-02 17:20:30 -0500

    updates

--- client/src/App.js
+++ client/src/App.js
@@ -6,9 +6,6 @@ import ToDoList from "./To-Do-List.js";
 function App () {
   return(
     <div>
-      <div>
-        APP JS
-      </div>
       <Container>
         <ToDoList/>
       </Container>
